86bb129df323e262deef29b755d6fa7dfb98c16b
DayaneCordeiro <dayane.cordeirogs@gmail.com>
2025-08-09 20:04:40 -0300

    feat: reduce

---
+++ ch5-reduce.clj
@@ -0,0 +1,20 @@
+;; verifica se o tipo da transação é despesa pela chave :tipo
+(defn despesa? [transacao]
+  (= (:tipo transacao) "despesa")) ; #'user/despesa?
+
+;; algumas transacoes
+(def transacoes
+  [{:valor 33.0 :tipo "despesa" :comentatio "Almoço" :data "19/11/2016"}
+   {:valor 2700.0 :tipo "receita" :comentatio "AlmBicooço" :data "01/12/2016"}
+   {:valor 29.0 :tipo "despesa" :comentatio "Livro de Clojure" :data "03/12/2016"}]) ; #'user/transacoes
+
+;; pega o campo valor da transacao
+(defn valor [transacao]
+  (:valor transacao)) ; #'user/valor
+
+;; pega a função valor e aplica à lista de despesas, de forma que vai mapear todos os valores
+;; de transações que são despesas
+(map valor (filter despesa? transacoes)) ; (33.0 29.0)
+
+;; aplica a operação de soma sobre as despesas
+(reduce + (map valor (filter despesa? transacoes))) ; 62.0
