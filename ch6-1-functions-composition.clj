b6413a88250fc2240e778504e837023519029fdc
DayaneCordeiro <dayane.cordeirogs@gmail.com>
2025-08-18 16:05:15 -0300

    enhancement: functions composition BigDecimal

--- ch6-1-functions-composition.clj
+++ ch6-1-functions-composition.clj
@@ -97,3 +97,36 @@
 
 (map texto-resumo-em-yuan transacoes) ; ("19/11/2016 => ¥ -70.95" "01/12/2016 => ¥ +5805.0" "03/12/2016 => ¥ -62.349999999999994")
 
+;; nota-se que a operação acima retornou uma dizima periódica
+;; para ter precisão é necessário utilizad o BigDecimal
+(class 3.1) ; java.lang.Double
+
+(* 3.1 3.1) ; 9.610000000000001
+
+(class 3.1M) ; java.math.BigDecimal <======
+
+(* 3.1M 3.1) ; 9.610000000000001
+
+(* 3.1M 3.1M) ; 9.61M
+
+(class (* 3.1M 3.1M)) ; java.math.BigDecimal
+
+;; ====================================================================
+(def cotacoes
+  {:yuan {:cotacao 2.15M :simbolo "¥"}})
+
+(def transacoes
+  [{:valor 33.0M :tipo "despesa" :comentatio "Almoço" :moeda "R$" :data "19/11/2016"}
+   {:valor 2700.0M :tipo "receita" :comentatio "Bico" :moeda "R$" :data "01/12/2016"}
+   {:valor 29.0M :tipo "despesa" :comentatio "Livro de Clojure" :moeda "R$" :data "03/12/2016"}])
+
+(map texto-resumo-em-yuan transacoes) ; ("19/11/2016 => ¥ -70.950" "01/12/2016 => ¥ +5805.000" "03/12/2016 => ¥ -62.350")
+
+(defn texto-resumo-em-yuan [transacao]
+  (data-transacao (transacao-em-yuan transacao)))
+
+;; Essa função é uma boa candidata á utilização da macro thread-first pois ela aplica a função
+;; data-transacao a cada valor retornado por transacao-em-yuan
+(defn texto-resumo-em-yuan [transacao]
+  (-> (transacao-em-yuan transacao)
+      (data-transacao)))
